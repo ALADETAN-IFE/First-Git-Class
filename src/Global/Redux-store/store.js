dd0ca8a9939fc4fc1e12e71a5f56959e07ec51cd
ALADETAN-IFE <163364168+ALADETAN-IFE@users.noreply.github.com>
2024-07-26 18:07:19 +0100

    I ran build

--- src/Global/Redux-store/store.js
+++ src/Global/Redux-store/store.js
@@ -4,6 +4,6 @@ import PlaylistReducer from "../Redux-actions/PlaylistSlice";
 
 export const store = configureStore({
   reducer: {
-    Playlist: PlaylistReducer,
+    Playlist:  PlaylistReducer,
   },
 });
